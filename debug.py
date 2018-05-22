59728aefa79ae068d06946dcbba69e86a8bd59fd
Simon Li <simon.li@yosoro.io>
2018-05-22 10:21:07 +1000

    fix index error

--- debug.py
+++ debug.py
@@ -8,5 +8,21 @@ def getDimensions(array):
     return (x, y)
 
 
+def getColorMode(array):
+    """Return the color mode of the image using pixel data"""
+    if len(array[0][0]) == 3:
+        return "RGB"
+    if len(array[0][0]) == 4:
+        return "RGBA"
+
+
 def saveNewBitmap(array, filename):
     """Saves a new bitmap image file. Debug use only."""
+    dimensions = getDimensions(array)
+    img = Image.new(getColorMode(array), dimensions)
+    for w in range(dimensions[0]):
+        for h in range(dimensions[1]):
+            img.putpixel((w, h), array[h][w])
+    img.save(filename)
+
+    
