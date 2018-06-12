72f6565775295cc049d52aeaddf41830250c5074
Simon Li <simon.li@yosoro.io>
2018-06-12 23:36:16 +1000

    find edge working

---
+++ path.py
@@ -0,0 +1,98 @@
+import helper as h
+
+question = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+]
+
+nine = [
+    [0, 1, 1],
+    [0, 0, 1],
+    [1, 0, 1],
+]
+
+
+def getCorner(tuple, binary):
+    """Returns four pixels of the given corner from (0, 0) in clockwise order."""
+    y = tuple[0]
+    x = tuple[1]
+    height, width = h.getDimensions(binary)
+    if y - height > 1 or x - width > 1:
+        raise IndexError("Out of index.")
+    if y < 0 or x < 0:
+        raise IndexError("Index must be postive.")
+    deltas = [(-1, -1), (-1, 0), (0, 0), (0, -1)]
+    adjacent_pixels = []
+    for delta in deltas:
+        try:
+            new_y = y+delta[0]
+            new_x = x+delta[1]
+            if new_y < 0 or new_x < 0:
+                pixel = 1
+            else:
+                pixel = binary[new_y][new_x]
+        except IndexError:
+            pixel = 1
+        adjacent_pixels.append(pixel)
+    return adjacent_pixels
+
+
+def getNextCorner(tuple, binary):
+    pixels = getCorner(tuple, binary)
+    points = []
+    if pixels[0] == 0 and pixels[1] == 1:
+        points.append((tuple[0] - 1, tuple[1]))
+    if pixels[1] == 0 and pixels[2] == 1:
+        points.append((tuple[0], tuple[1] + 1))
+    if pixels[2] == 0 and pixels[3] == 1:
+        points.append((tuple[0]+1, tuple[1]))
+    if pixels[3] == 0 and pixels[0] == 1:
+        points.append((tuple[0], tuple[1] - 1))
+    return points
+
+
+def findEdge(tuple, binary, path=[]):
+    path = path
+    if len(path) == 0:
+        return findEdge(tuple, binary, path=[(tuple)])
+    if len(path) > 1 and path[0] == path[-1]:
+        return path
+    else:
+        next_points = getNextCorner(tuple, binary)
+        for point in next_points:
+            if point in path:
+                previous_index = path.index(point)-1
+                if previous_index > -1:
+                    if path[previous_index] == tuple:
+                        next_points.remove(point)
+            next_point = next_points[0]
+        path.append(next_point)
+        return findEdge(next_point, binary, path=path)
+
+
+if __name__ == "__main__":
+    a = findEdge((2, 2), nine)
+    print(a)
